4c62ce41e4763686073e34c26edb7ca4f76f479a
Ehab Amer <ehab.amer@gmail.com>
2022-07-21 13:14:59 +0200

    Update rome.rb

--- rome.rb
+++ rome.rb
@@ -1,7 +1,7 @@
 class Rome < Formula
   desc "A shared cache tool for Carthage"
-  homepage "https://github.com/blender/Rome"
-  url "https://github.com/blender/Rome/releases/download/v0.24.0.66/rome.zip"
+  homepage "https://github.com/Waheiba/Rome"
+  url "https://github.com/Waheiba/Rome/releases/download/v0.24.0.66/rome.zip"
   sha256 "4d40b46fd350b7ca0d63540d6248067d54cd8183cbd7e4eaf9b032b60da3d96b"
 
   def install
